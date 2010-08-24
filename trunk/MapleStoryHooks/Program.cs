2400e9b033b2b8a3f374a939e055670d5ac9e175
superraz375@gmail.com <superraz375@gmail.com@591bd7ce-346b-f270-ee08-dd7029cce22d>
2010-08-24 20:33:25 +0000

    - i think this works

--- trunk/MapleStoryHooks/Program.cs
+++ trunk/MapleStoryHooks/Program.cs
@@ -7,23 +7,64 @@ using EasyHook;
 using System.Runtime.Remoting;
 using System.Threading;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
 
 namespace MapleStoryHooks
 {
-   
-
-
-
-    public class Program
+    public class MapleStoryHookInterface : MarshalByRefObject
     {
-        [STAThread]
-        private static void Main(string[] pArgs)
+        public void IsInstalled(Int32 InClientPID)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.WriteLine("Hook installed in target {0}.\r\n", InClientPID);
         }
 
+        public void WriteConsole(string s)
+        {
+            Console.WriteLine("[DLL] {0}", s);
+        }
+    }
+
+
+    
+    public class Program
+    {
+        static String ChannelName = null;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MapleStory Hooksss");
+            Console.WriteLine();
+            Console.WriteLine("Input process name...");
+            
+            string processName = Console.ReadLine();
+
+            bool lookingForProcess = true;
+            while (lookingForProcess)
+            {
+                //Process[] processes = Process.GetProcessesByName("WvsLogin");
+                Process[] processes = Process.GetProcessesByName(processName);
+
+
+                if (processes.Length > 0)
+                {
+                    Console.WriteLine("Found process.");
+                    int pid = processes[0].Id;
+                    string path = @"MapleStoryHooks.dll";
+                    string path2 = @"MapleStoryHooks.exe";
+
+                    Config.Register("Maplestory hook", path, path2);
+
+                    RemoteHooking.IpcCreateServer<MapleStoryHookInterface>(ref ChannelName, WellKnownObjectMode.SingleCall);
+                    RemoteHooking.Inject(pid, path, path, ChannelName);
+                    lookingForProcess = false;
+                }
+                else
+                {
+                    Console.WriteLine("Cannot find process.");
+                    Thread.Sleep(500);
+                }
+            }
+
+            Console.ReadLine();
+        }
     }
 }
